cf9d2547962d935b31e9d837f7b31b6642589872
Imamasharmeen <imamasharmeen@gmail.com>
2024-10-03 13:03:37 +0300

    Added video card using daisy UI

--- script/video.js
+++ script/video.js
@@ -6,6 +6,42 @@ const loadCategories = () => {
     .catch((error) => console.log(error))
 };
 
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+
+};
+
+const displayVideos = (videos) =>{
+    //console.log(videos);
+    const videoContainer = document.getElementById('videos')
+
+    videos.forEach(video => {
+        console.log(video)
+        const card = document.createElement('div');
+        card.classList = 'card card-compact'
+        card.innerHTML=`
+        <figure>
+            <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        videoContainer.append(card)
+    })
+}
+
+    
+
 
 
 //create displayCategories 
@@ -13,7 +49,7 @@ const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('categories')
 
     categories.forEach(item => {
-        console.log(item);
+        //console.log(item);
 
         const button = document.createElement('button');
         button.classList = 'btn';
@@ -23,5 +59,6 @@ const displayCategories = (categories) => {
     });
 };
 loadCategories()
-displayVideo()
+loadVideos()
+
 
